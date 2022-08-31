6de58e739fa57aa961eec4552234c8c3e2653c4f
Jacob Root <otis@otisroot.com>
2022-08-30 22:31:29 -0700

    Add testing for channel rm and re-create cog

--- bot/cogs/malding.py
+++ bot/cogs/malding.py
@@ -10,6 +10,13 @@ class Cog(commands.Cog, name="malding"):
     def __init__(self, bot):
         self.bot = bot
 
+    async def cog_load(self) -> None:
+        now = datetime.datetime.now()
+        time = now.replace(hour=24)
+        diff = time - now
+        secs = diff.total_seconds()
+        print(secs)
+
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.guild.id == 964451976186310668 and message.channel.id == 1014390289181446174:
