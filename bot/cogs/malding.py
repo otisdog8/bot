0b787e53e035015862cff9e003dde43b2f053c47
Jacob Root <otis@otisroot.com>
2022-08-30 22:38:18 -0700

    Use UTC

--- bot/cogs/malding.py
+++ bot/cogs/malding.py
@@ -12,7 +12,7 @@ class Cog(commands.Cog, name="malding"):
 
     async def cog_load(self) -> None:
         now = datetime.datetime.now()
-        time = now.replace(hour=23)
+        time = now.replace(hour=7)
         diff = time - now
         secs = diff.total_seconds()
         print(secs)
