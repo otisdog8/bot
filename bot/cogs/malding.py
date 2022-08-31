f3ed3a1d6944f074b1b79533763a1cf814aa1518
Jacob Root <otis@otisroot.com>
2022-08-30 22:35:24 -0700

    Fix

--- bot/cogs/malding.py
+++ bot/cogs/malding.py
@@ -12,7 +12,7 @@ class Cog(commands.Cog, name="malding"):
 
     async def cog_load(self) -> None:
         now = datetime.datetime.now()
-        time = now.replace(hour=0) + datetime.timedelta(days=1)
+        time = now.replace(hour=23)
         diff = time - now
         secs = diff.total_seconds()
         print(secs)
