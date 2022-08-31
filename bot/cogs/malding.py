d560d1cacdc48a1d16d7230269046eef5b665c9c
Jacob Root <otis@otisroot.com>
2022-08-30 23:01:21 -0700

    Remove await

--- bot/cogs/malding.py
+++ bot/cogs/malding.py
@@ -25,7 +25,7 @@ class Cog(commands.Cog, name="malding"):
         #await asyncio.sleep(secs)
         while True:
             # Delete and recreate
-            guild = await self.bot.get_guild(guildId)
+            guild = self.bot.get_guild(guildId)
             print(guild)
             for channel in guild.channels:
                 print(channel.name)
